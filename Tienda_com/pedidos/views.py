d7d17f5409c3ecf13f471f1b8cd82ed6f7a0de2e
RitchiDV <102975634+RitchiDV@users.noreply.github.com>
2022-06-14 09:38:00 -0500

    Delete Tienda_com directory

--- Tienda_com/pedidos/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from pedidos.models import Pedido
-
-# Create your views here.
-def pedidos (request):
-    pedido=Pedido.objects.all()
-
-     
-    return render(request,"pedidos/pedidos.html",{"pedido":pedido})
--- Tienda_com/servicios/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from servicios.models import Servicio
-
-def services(request):
-
-
-    servicio = Servicio.objects.all()
-
-    return render(request,"servicios/services.html",{"servicio":servicio})
